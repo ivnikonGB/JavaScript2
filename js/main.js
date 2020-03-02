f20999c63a2a9df8784a8c85e5e64eb3ba018c47
ivnikonGB <ivnikon@yandex.ru>
2020-03-02 15:28:57 +0300

    Lesson1 done

--- js/main.js
+++ js/main.js
@@ -3,17 +3,19 @@ const products = [
     {id: 2, title: 'Mouse', price: 20},
     {id: 3, title: 'Keyboard', price: 200},
     {id: 4, title: 'Gamepad', price: 50},
+    {id: 5, title: 'Test'}
 ];
 
-const renderProduct = (title, price) => {
+const renderProduct = (title="Нет данных", price="н/д") => {
     return `<div class="product-item">
                 <h3>${title}</h3>
+                <img src="http://placeimg.com/200/150/tech">
                 <p>${price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
 };
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title, item.price));
+    const productsList = list.map(item => renderProduct(item.title, item.price)).join("");
     console.log(productsList);
     document.querySelector('.products').innerHTML = productsList;
 };
