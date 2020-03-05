e28d51bbb86fcec252d4793690c0eeea4af6d8e9
ivnikonGB <ivnikon@yandex.ru>
2020-03-05 16:45:38 +0300

    lesson2 done

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ class ProductsList {
         this.container = container;
         this.goods = [];
         this.allProducts = [];
-        this._fetchProducts()
+        this._fetchProducts();
     }
 
     _fetchProducts() {
@@ -22,6 +22,14 @@ class ProductsList {
             this.allProducts.push(productObj);
             block.insertAdjacentHTML('beforeend', productObj.render());
         }
+    }
+
+    getSumm() {
+        let summ = 0;
+        for(let product of this.allProducts) {
+            summ += product.price;
+        }
+        return summ;
     }
 }
 
@@ -45,31 +53,19 @@ class ProductItem {
 
 let list = new ProductsList();
 list.render();
+console.log(list.getSumm());
 
+class Cart {
+    constructor() {};
+    addProduct(productId) {};
+    clearCart() {};
+    checkoutCart() {};
+    getSumm() {};
+    render() {};
+}
 
-
-
-
-// const products = [
-//     {id: 1, title: 'Notebook', price: 2000},
-//     {id: 2, title: 'Mouse', price: 20},
-//     {id: 3, title: 'Keyboard', price: 200},
-//     {id: 4, title: 'Gamepad', price: 50},
-//     {id: 5, title: 'Test'}
-// ];
-
-// const renderProduct = (title="Нет данных", price="н/д") => {
-//     return `<div class="product-item">
-//                 <h3>${title}</h3>
-//                 <img src="http://placeimg.com/200/150/tech">
-//                 <p>${price}</p>
-//                 <button class="buy-btn">Купить</button>
-//             </div>`
-// };
-// const renderPage = list => {
-//     const productsList = list.map(item => renderProduct(item.title, item.price)).join("");
-//     console.log(productsList);
-//     document.querySelector('.products').innerHTML = productsList;
-// };
-
-// renderPage(products);
+class CartItem {
+    constructor() {};
+    removeFromCart(productId) {};
+    render() {};
+}
